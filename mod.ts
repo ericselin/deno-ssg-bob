180f1f0c132622345ab91ebfa4c0b9ac39c55c53
Eric Selin <eric.selin@gmail.com>
2021-10-30 15:22:12 +0200

    types: Export `ContentBase` and `ContentUnknown` from `mod.ts`
    These types are likely needed in site implementations.

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export type { Component, Props } from "./domain.ts";
+export type { Component, Props, ContentBase, ContentUnknown } from "./domain.ts";
 export { build } from "./core/api.ts";
 export { h } from "./core/jsx.ts";
 export * from "./utils/get-path.ts";
