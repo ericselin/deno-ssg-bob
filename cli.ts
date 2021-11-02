a328f4365daac39e914615a05ccce05c8fd5bf71
Eric Selin <eric.selin@gmail.com>
2021-11-02 19:11:21 +0100

    fix: Reload layouts when running in server mode
    In order to reload modules (i.e. layouts) when in server mode, a separate process is now spawned to run the build. This process uses the `bob` executable, which means bob needs to be installed in order for this to work. The re-build uses the same command line arguments as the parent process, minus the `server` argument, of course.
    
    Spawning a process is a separate permission in Deno, `--allow-run`. This needs to be allowed on the installed bob instance.
    
    The reason for this is that ES modules are (by specification) cached for the duration of the process. In order to really reload everything, a new process needs to be spawned. Another possibility would have been to add a search- or hash-string to the module loading path, but that has two problems: i) it's pretty cumbersome to set up correctly, and ii) it doesn't work for changes in modules down the dependency graph (i.e. only works for the actually loaded module, which is the layout).
    
    When Deno implements hooks for module resolution, this spawning can be avoided.

--- cli.ts
+++ cli.ts
@@ -28,7 +28,9 @@ const {
   d: buildDrafts,
   _: [arg],
 } = parseFlags(Deno.args);
-const server = arg === "server";
+
+const SERVER_ARG = "server";
+const server = arg === SERVER_ARG;
 
 if (help) {
   console.log(usage);
@@ -62,10 +64,39 @@ const buildOptions: BuildOptions = {
 await build(buildOptions);
 
 if (server) {
+  // Start HTTP server of public folder
   serve({ directory: "public", log });
-  const watcher = Deno.watchFs([ buildOptions.layoutDir, buildOptions.contentDir ]);
+
+  /** File watcher for content and layout dirs. */
+  const watcher = Deno.watchFs([
+    buildOptions.layoutDir,
+    buildOptions.contentDir,
+  ]);
+  /** Timer id of `setTimeout` used for debouncing. */
+  let runningTimer = 0;
+  /**
+  Command to run. Always run via the `bob` executable.
+  Pass in original arguments, except for the "server" argument.
+  */
+  const cmd = ["bob", ...Deno.args.filter((arg) => arg !== SERVER_ARG)];
+
   for await (const event of watcher) {
-    log.info(`Files changed: ${event.kind} ${event.paths}`);
-    await build(buildOptions);
+    // Don't build on access events
+    if (event.kind !== "access") {
+      log.info(`Files changed: ${event.kind} ${event.paths}`);
+      // Continue only if we're not already waiting for a build to begin
+      if (!runningTimer) {
+        log.debug("Starting timeout for building");
+        // Start timeout for build
+        runningTimer = setTimeout(async () => {
+          // Spawn new process for re-building. This makes Deno check for updated
+          // modules (i.e. layouts) and reloads them as neccessary.
+          const buildProcess = Deno.run({ cmd });
+          await buildProcess.status();
+          // Remember to reset the timer id
+          runningTimer = 0;
+        }, 100);
+      }
+    }
   }
 }
