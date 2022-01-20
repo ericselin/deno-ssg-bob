f821ff17980a6b722ff20da27e7c7bf6b665f13d
Eric Selin <eric.selin@gmail.com>
2022-01-20 14:27:04 +0100

    Add content writer to server funcion context
    This writer will write content and wait for rendering to complete.

--- docs/functions/index.ts
+++ docs/functions/index.ts
@@ -5,7 +5,19 @@ const testHandler: FunctionHandler = (_req, ctx) => {
   return new Response(`You requested test in folder ${ctx.pathnameParams.dir}`);
 };
 
+const writerHandler: FunctionHandler = async (req, ctx) => {
+  const contents = new URL(req.url).searchParams.get("c");
+  // update contents e.g. writeContentFile(relativePath, contents: string)
+  await ctx.writeAndRender("test", contents || "[empty]");
+  // send response
+  return new Response("Redirecting...", {
+    status: 302,
+    headers: { Location: "/test" },
+  });
+};
+
 export default [
   ["/hello", helloHandler],
   ["/:dir/test", testHandler],
+  ["/write", writerHandler],
 ];
--- functions/mod.ts
+++ functions/mod.ts
@@ -1,15 +1,21 @@
-import type { Logger } from "../domain.ts";
+import type { BuildOptions, Change, Logger } from "../domain.ts";
 import { loadIfExists } from "../core/module-loader.ts";
-import { serve as listen } from "../deps.ts";
+import { fs, path, serve as listen } from "../deps.ts";
+import { getChanger } from "../core/api.ts";
 const FUNCTIONS_PATH = "functions/index.ts";
 
 export type FunctionHandler = (
   req: Request,
   ctx: FunctionContext,
 ) => Response | Promise<Response>;
+
 type FunctionContext = {
   pathnameParams: Record<string, string>;
+  writeAndRender: ContentWriter;
 };
+
+type ContentWriter = (contentPath: string, content: string) => Promise<void>;
+
 type FunctionDefinition = [string, FunctionHandler];
 type FunctionDefinitions = FunctionDefinition[] | undefined;
 type Functions = [URLPattern, FunctionHandler][];
@@ -17,10 +23,30 @@ type Functions = [URLPattern, FunctionHandler][];
 type FunctionServerOptions = {
   log?: Logger;
   port?: number;
+  buildOptions: BuildOptions;
+};
+
+const getContentWriter = (buildOptions: BuildOptions): ContentWriter => {
+  const applyChange = getChanger(buildOptions);
+  return async (contentPath, content) => {
+    // get content path
+    const filepath = path.join(buildOptions.contentDir, contentPath);
+    // ensure directory exists
+    await fs.ensureDir(path.dirname(filepath));
+    // write file
+    await Deno.writeTextFile(filepath, content);
+    // create change
+    const change: Change = {
+      type: "modify",
+      inputPath: filepath,
+    };
+    // apply change
+    await applyChange(change);
+  };
 };
 
 export const serve = async (options: FunctionServerOptions) => {
-  const { log, port } = Object.assign(
+  const { log, port, buildOptions } = Object.assign(
     options,
     { port: 8081 } as FunctionServerOptions,
   );
@@ -35,13 +61,15 @@ export const serve = async (options: FunctionServerOptions) => {
   const functions: Functions = functionDefinitions.map((
     [pathname, handler],
   ) => [new URLPattern({ pathname }), handler]);
-  // start server for each function
+  // create content writer
+  const writeAndRender = getContentWriter(buildOptions);
+  // start server
   listen((request) => {
     const fn = functions.find(([pattern]) => pattern.test(request.url));
     if (!fn) return new Response("Not found", { status: 404 });
     const [pattern, handler] = fn;
     const match = pattern.exec(request.url)!;
-    return handler(request, { pathnameParams: match.pathname.groups });
+    return handler(request, { pathnameParams: match.pathname.groups, writeAndRender });
   }, { port });
   log?.info(`Functions running on port ${port}`);
 };
